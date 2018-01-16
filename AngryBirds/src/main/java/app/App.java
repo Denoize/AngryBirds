4b9a71a65cf5e4b8191a693bc4d376d95cb392d0
Denoize <aurelien.denoize@outlook.com>
2018-01-16 17:11:50 +0100

    App + Menu

--- AngryBirds/src/main/java/app/App.java
+++ AngryBirds/src/main/java/app/App.java
@@ -1,5 +1,35 @@
 package main.java.app;
 
-public class App {
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class App extends Frame{
+	
+
+	private static final long serialVersionUID = 1L;
+	public static Graphics graphic;
+	
+	public App(){
+	      super("Java AWT Examples");
+	      prepareGUI();
+	   }
+
+	   public static void main(String[] args){
+		   App  awtGraphicsDemo = new App();  
+	      awtGraphicsDemo.setVisible(true);
+	   }
+
+	   private void prepareGUI(){
+	      setSize(800,600);
+	      addWindowListener(new WindowAdapter() {
+	         public void windowClosing(WindowEvent windowEvent){
+	            System.exit(0);
+	         }        
+	      }); 
+	      
+	      graphic = getGraphics();
+	   }    
 
 }
--- AngryBirds/src/main/java/service/Menu.java
+++ AngryBirds/src/main/java/service/Menu.java
@@ -1,5 +1,65 @@
 package main.java.service;
 
-public class Menu {
+import java.awt.Panel;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
+import main.java.model.Player;
+
+public class Menu extends Panel implements  MouseListener, MouseMotionListener{
+
+	private static final long serialVersionUID = 1L;
+
+
+	public Menu(){
+		
+	}
+	
+	
+	/**
+	 * Creer un nouveau joueur
+	 * @return
+	 */
+	public Player newPlayer() {
+		
+		Player player = new Player();
+		
+		
+		
+		return null;
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void newGame() {
+		
+	}
+	
+	public Player loadPlayer() {
+		return null;
+		
+	}
+	
+	public void quit() {
+		
+	}
+	
+	public void mouseClicked(MouseEvent e) { }
+    public void mouseEntered(MouseEvent e) { 
+    	//if(e.getX() > 100 && e.getX() < 200)
+    		
+    }
+    public void mouseExited(MouseEvent e)  { }
+    public void mousePressed(MouseEvent e) { }
+    public void mouseReleased(MouseEvent e) {}
+    public void mouseDragged(MouseEvent e) { mouseMoved(e); }
+    public void mouseMoved(MouseEvent e) { 
+        
+        repaint();
+    }
+	
+	
 }
