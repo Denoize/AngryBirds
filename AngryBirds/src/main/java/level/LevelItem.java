7933f033c008857a37d14766071a1462c5441377
b21108084 <b21108084@L-05402300F2-04.salsa.univ-amu.fr>
2018-01-16 17:10:06 +0100

    images + MAJ LevelItem

--- AngryBirds/src/main/java/level/LevelItem.java
+++ AngryBirds/src/main/java/level/LevelItem.java
@@ -1,5 +1,40 @@
 package main.java.level;
 
-public abstract class LevelItem {
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+public abstract class LevelItem extends Component{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private BufferedImage img;
+	private Point position;
+
+    public void paint(Graphics g) {
+       g.drawImage(img, (int) position.getX(), (int) position.getY(), null);
+    }
+
+    public LevelItem(String path) {
+       try {
+          img = ImageIO.read(new File(path));
+       } catch (IOException e) {
+          e.printStackTrace();
+       }
+    }
+
+    public Dimension getPreferredSize() {
+       if (img == null) {
+          return new Dimension(100,100);
+       } else {
+          return new Dimension(img.getWidth(), img.getHeight());
+       }
+    }
 }
